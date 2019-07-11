b7bb60b2e8527d0366385f42cc9f2207ea9bf118
唐桂富 <tangguifu@192.168.6.120>
2019-07-11 23:45:22 +0800

    原生工程交付

---
+++ EBAG/android/app/src/main/java/com/ebag/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ebag;
+import cn.jpush.android.api.JPushInterface;
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "EBAG";
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        JPushInterface.init(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        JPushInterface.onPause(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        JPushInterface.onResume(this);
+    }
+}
