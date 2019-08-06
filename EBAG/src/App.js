390fef1939c77b1cf7a9d6b38cd46a12d9595dd3
唐桂富 <tangguifu@192.168.0.105>
2019-08-07 07:59:20 +0800

    将更新方案有sync换成cheeckForUpdate

--- EBAG/src/App.js
+++ EBAG/src/App.js
@@ -51,33 +51,41 @@ class App extends Component {
   }
   //如果有更新的提示
   syncImmediate() {
-    CodePush.sync( {
-          //安装模式
-          //ON_NEXT_RESUME 下次恢复到前台时
-          //ON_NEXT_RESTART 下一次重启时
-          //IMMEDIATE 马上更新
-          installMode : CodePush.InstallMode.ON_NEXT_RESTART ,
-          //对话框
-          updateDialog : {
-            //是否显示更新描述
-            appendReleaseDescription : true ,
-            //更新描述的前缀。 默认为"Description"
-            descriptionPrefix : "更新内容：" ,
-            //强制更新按钮文字，默认为continue
-            mandatoryContinueButtonLabel : "立即更新" ,
-            //强制更新时的信息. 默认为"An update is available that must be installed."
-            mandatoryUpdateMessage : "必须更新后才能使用" ,
-            //非强制更新时，按钮文字,默认为"ignore"
-            optionalIgnoreButtonLabel : '稍后' ,
-            //非强制更新时，确认按钮文字. 默认为"Install"
-            optionalInstallButtonLabel : '后台更新' ,
-            //非强制更新时，检查到更新的消息文本
-            optionalUpdateMessage : '有新版本了，是否更新？' ,
-            //Alert窗口的标题
-            title : '更新提示'
-          } ,
-        } ,
-    );
+    CodePush.checkForUpdate(CODE_PUSH_KEY).then((update) => {
+      console.log('-------' + update)
+      if (!update) {
+        Toast.showLongSuccess('已是最新版本！')
+      } else {
+        
+      }
+    })
+    // CodePush.sync( {
+    //       //安装模式
+    //       //ON_NEXT_RESUME 下次恢复到前台时
+    //       //ON_NEXT_RESTART 下一次重启时
+    //       //IMMEDIATE 马上更新
+    //       installMode : CodePush.InstallMode.IMMEDIATE ,
+    //       //对话框
+    //       updateDialog : {
+    //         //是否显示更新描述
+    //         appendReleaseDescription : true ,
+    //         //更新描述的前缀。 默认为"Description"
+    //         descriptionPrefix : "更新内容：" ,
+    //         //强制更新按钮文字，默认为continue
+    //         mandatoryContinueButtonLabel : "立即更新" ,
+    //         //强制更新时的信息. 默认为"An update is available that must be installed."
+    //         mandatoryUpdateMessage : "必须更新后才能使用" ,
+    //         //非强制更新时，按钮文字,默认为"ignore"
+    //         optionalIgnoreButtonLabel : '稍后' ,
+    //         //非强制更新时，确认按钮文字. 默认为"Install"
+    //         optionalInstallButtonLabel : '后台更新' ,
+    //         //非强制更新时，检查到更新的消息文本
+    //         optionalUpdateMessage : '有新版本了，是否更新？' ,
+    //         //Alert窗口的标题
+    //         title : '更新提示'
+    //       } ,
+    //     } ,
+    // );
   }
 
   componentWillMount() {
